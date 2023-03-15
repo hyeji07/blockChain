5bf7cf9391f537fad3340f9bcd0075e791b20d51
hyeji07 <zindex_ann@naver.com>
2023-03-15 15:29:08 +0900

    wallet mobile 진행중

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,6 +1,10 @@
+import axios from 'axios';
+
 import { useRecoilState } from 'recoil';
-import { walletAtom } from '@stores/wallet/wallet';
+import { walletKey } from '@stores/wallet/wallet';
 import Caver from 'caver-js';
+
+import { browserName, browserVersion } from 'react-device-detect';
 
 /* import { Toolbar, Typography, IconButton, InputBase } from '@mui/material';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
@@ -12,33 +16,53 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'; */
 
 import CreateBtn from '../../btns/CreateBtn';
 
+console.log(`${browserName} ${browserVersion}`);
+
+export function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
+}
+
 export default function Header() {
-  const [walletState, setWalletState] = useRecoilState(walletAtom);
+  const [walletState, setWalletState] = useRecoilState(walletKey);
+
+  const onSignOut = () => {
+    setWalletState(null);
+    //localStorage.removeItem('walletKey');
+    //window.location.reload();
+  };
 
   //Kaikas
   const caver = new Caver(window.klaytn);
 
   const handleOpenKaikas = async () => {
+    /* if (isMobile()) {
+      connectMobile();
+      return;
+    } */
+
     if (window.klaytn) {
+      const accounts = await window.klaytn.enable();
+      const account = accounts[0];
+
       //접속
       alert('카이카스를 연결하겠습니다.');
 
-      const accounts = await window.klaytn.enable();
-      const account = accounts[0];
-      console.log(account);
-
       //사용자 계정에 접근하기
       window.klaytn.on('accountsChanged', function (accounts) {
-        console.log(window.klaytn.selectedAddress);
+        //console.log(window.klaytn.selectedAddress);
         setWalletState(window.klaytn.selectedAddress);
       });
 
       //사용자 네크워크 정보 가져오기
       window.klaytn.on('networkChanged', function () {
-        console.log(window.klaytn.networkVersion);
+        //console.log(window.klaytn.networkVersion);
       });
       setWalletState(window.klaytn.selectedAddress);
-    } else {
+    }
+
+    if (!window.klaytn) {
       alert('클레이튼 지갑을 설치해주세요.');
       window.open(
         'https://chrome.google.com/webstore/detail/kaikas/jblndlipeogpafnldhgmapagcccfchpi',
@@ -50,31 +74,57 @@ export default function Header() {
   //Metamask
   const handleOpenMetamask = async () => {
     if (window.ethereum) {
+      const accounts = await window.ethereum.enable();
+      const account = accounts[0];
+
       //접속
       alert('메타마스크를 연결하겠습니다.');
 
-      const accounts = await window.ethereum.enable();
-      const account = accounts[0];
-      console.log(account);
-
       //사용자 계정에 접근하기
       window.ethereum.on('accountsChanged', function (accounts) {
-        console.log(window.ethereum.selectedAddress);
+        //console.log(window.ethereum.selectedAddress);
         setWalletState(window.ethereum.selectedAddress);
       });
 
       //사용자 네크워크 정보 가져오기
       window.ethereum.on('networkChanged', function () {
-        console.log(window.ethereum.networkVersion);
+        //console.log(window.ethereum.networkVersion);
       });
       setWalletState(window.ethereum.selectedAddress);
-    } else {
+    }
+
+    if (!window.klaytn) {
       alert('메타마스크 지갑을 설치해주세요.');
       window.open(
         'https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn',
         '_blank'
       );
     }
+  };
+
+  //Mobile
+  const connectMobile = async () => {
+    const isSafariMobile = 'Mobile Safari' === browserName;
+    const isChrome = 'Chrome' === browserName;
+
+    //
+    try {
+      const body = {
+        type: 'auth',
+        bapp: {
+          name: 'test app',
+          callback: {
+            success: 'https://www.google.com/search?q=success',
+            fail: 'https://www.google.com/search?q=fail',
+          },
+        },
+      };
+      const result = await axios.post(
+        'https://api.kaikas.io/api/v1/k/prepare',
+        body
+      );
+      console.log(result);
+    } catch {}
   };
 
   return (
@@ -85,24 +135,52 @@ export default function Header() {
           {walletState ? (
             <div>
               <p>{walletState}</p>
+              <p>
+                <CreateBtn
+                  text={'Logout'}
+                  onClick={onSignOut}
+                  className='noticeUpdateBtn'
+                />
+              </p>
             </div>
           ) : (
-            <div>
+            <>
               <div>
-                <CreateBtn
-                  text={'kaikas 연결'}
-                  onClick={handleOpenKaikas}
-                  className='noticeUpdateBtn'
-                />
+                <p>Web</p>
+                <div>
+                  <CreateBtn
+                    text={'kaikas 연결'}
+                    onClick={handleOpenKaikas}
+                    className='noticeUpdateBtn'
+                  />
+                </div>
+                <div>
+                  <CreateBtn
+                    text={'metamask 연결'}
+                    onClick={handleOpenMetamask}
+                    className='noticeUpdateBtn'
+                  />
+                </div>
               </div>
+              {/* Mobile */}
               <div>
-                <CreateBtn
-                  text={'metamask 연결'}
-                  onClick={handleOpenMetamask}
-                  className='noticeUpdateBtn'
-                />
+                <p>Mobile</p>
+                <div>
+                  <CreateBtn
+                    text={'kaikas 연결'}
+                    onClick={connectMobile}
+                    className='noticeUpdateBtn'
+                  />
+                </div>
+                {/*  <div>
+                  <CreateBtn
+                    text={'metamask 연결'}
+                    onClick={handleOpenMetamask}
+                    className='noticeUpdateBtn'
+                  />
+                </div> */}
               </div>
-            </div>
+            </>
           )}
         </div>
       </header>
--- src/stores/wallet/wallet.ts
+++ src/stores/wallet/wallet.ts
@@ -1,11 +1,14 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
 
-const { persistAtom } = recoilPersist();
+const { persistAtom } = recoilPersist({
+  key: 'walletKey',
+  storage: localStorage,
+});
 
 //Kaikas
-export const walletAtom = atom({
-  key: 'walletAtom',
+export const walletKey = atom({
+  key: 'walletKey',
   default: null,
-  /* effects_UNSTABLE: [persistAtom], */
+  effects_UNSTABLE: [persistAtom],
 });
